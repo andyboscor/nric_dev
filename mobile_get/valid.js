bac500558df28c8df88e448e676d7081754aa25f
k-i-k-ichi <k-i-k-ichi@users.noreply.github.com>
2016-03-14 10:11:16 +0000

    checking returned json is valid

--- mobile_get/valid.js
+++ mobile_get/valid.js
@@ -1,9 +1,10 @@
 
-function valid()
+function valid(user_object)
 {
 	var user=document.login.usermail.value;
 	var user=user.trim();
 	var pass=document.login.password.value;
+	
 	if(user == '')
 	{
 		document.getElementById('error').innerHTML="Please Enter Username";
@@ -14,5 +15,10 @@ function valid()
 		document.getElementById('error').innerHTML="Please Enter Password";
 		return false;
 	}
-	return true;
+	// json has sessID if login sucessful
+	if(user_object.hasOwnProperty('sessid')){
+		return true;
+	}
+
+	return false;
 }
