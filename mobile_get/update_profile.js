0dbe9349346debad471a4a441442426eeb153a31
k-i-k-ichi <k-i-k-ichi@users.noreply.github.com>
2016-04-18 18:28:58 +0100

    Fix illegal choice issue

--- mobile_get/update_profile.js
+++ mobile_get/update_profile.js
@@ -1,20 +1,47 @@
-function update_filter(filters /*array of string*/,csrf-token, uid){
-    
-    var obj = "{\"field_placement_settings\":{\"und\":{";
-       
-        // \"Settings-Community\":\"Settings-Community\"" 
+function update_filter_settings(filters /*array of string*/,csrf-token, uid){
+  
+    // Prepare the object
+    var obj = { };
+      
+     
+    // Seperate filters by type 
+    for (var i =0; i < len; i++){
+        var key = filters[i];
         
-        if (filters.length() == 1){
-            object+= filters;
-        }
-        else{ 
-            for (var i in filters)
+        // Handle region category
+        if (key.indexOf("-") == -1)
+        {
+            // Check if category already exist in object
+            if (!("field_region" in obj))
             {
-                obj += "\"" + i + "\":\"" + i + "\",";
-                 
+                // If not makeone
+                obj["field_region"] = { };
+                obj["field_region"]["und"] = { };  
             }
+            // Append key to category
+            obj["field_region"]["und"][key]=key;  
         }
-    obj+="}}}";
+
+        // For the rest
+        else 
+        {
+           // Get the category name
+            var i = key.indexOf('-');
+            var type = key.subString(0, i);
+            type = type.toLowerCase();
+
+            // Check if category exist
+            if (!("field_placement_"+type in obj))
+            {
+                obj["field_placement_"+type] = { };
+                obj["field_placement_"+type]["und"] = { }; 
+            }
+            // Append new key
+            obj["field_placement_"+type]["und"][key] = key;
+        } 
+    }
+     
+   
     
     $.ajax({
         url: "http://139.162.199.80/nricrestapi/user/"+uid,
@@ -22,7 +49,7 @@ function update_filter(filters /*array of string*/,csrf-token, uid){
         headers:{'X-CSRF-Token': csrf-token};
         dataType: 'json',
         contentType: 'application/json',
-        data:ojb,
+        data: obj,
         crossDomain:true,
         async:false,
         error: function(errorThrown){
@@ -36,3 +63,4 @@ function update_filter(filters /*array of string*/,csrf-token, uid){
         }
     }) 
 }
+
